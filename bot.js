e5582178c05313211a78a48d496d3ddbc03ac520
Pranay Pandey <pranay.pnd@gmail.com>
2020-06-19 08:57:29 +0530

    removed dialogflow

--- bot.js
+++ bot.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const Quote = require('inspirational-quotes');
 const movieQuote = require("popular-movie-quotes");
-const dialogflow = require('dialogflow');
-const uuid = require('uuid');
 
 var settings = require("./settings");
 
@@ -16,46 +14,6 @@ var voiceConnection;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
-function getDialogFlowResponse() {
-    /**
-     * Send a query to the dialogflow agent, and return the query result.
-     * @param {string} projectId The project to be used
-     */
-    async function runSample(projectId = process.env.PROJECT_ID) {
-        // A unique identifier for the given session
-        const sessionId = uuid.v4();
-
-        // Create a new session
-        const sessionClient = new dialogflow.SessionsClient();
-        const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-        // The text query request.
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    // The query to send to the dialogflow agent
-                    text: 'hello',
-                    // The language used by the client (en-US)
-                    languageCode: 'en-US',
-                },
-            },
-        };
-
-        // Send request and log result
-        const responses = await sessionClient.detectIntent(request);
-        console.log('Detected intent');
-        const result = responses[0].queryResult;
-        console.log(`  Query: ${result.queryText}`);
-        console.log(`  Response: ${result.fulfillmentText}`);
-        if (result.intent) {
-            console.log(`  Intent: ${result.intent.displayName}`);
-        } else {
-            console.log(`  No intent matched.`);
-        }
-    }
-}
 
 function getUserFromMention(mention) {
     if (!mention) return;
